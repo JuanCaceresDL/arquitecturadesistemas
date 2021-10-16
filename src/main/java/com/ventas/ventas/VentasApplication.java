10f22276ec431b2e2d212616e069a351bd6211a8
JuanCaceresDL <41441022+JuanCaceresDL@users.noreply.github.com>
2021-10-15 23:40:03 -0600

    Update VentasApplication.java

--- src/main/java/com/ventas/ventas/VentasApplication.java
+++ src/main/java/com/ventas/ventas/VentasApplication.java
@@ -2,13 +2,12 @@ package com.ventas.ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VentasApplication extends SpringBootServletInitializer{
+public class VentasApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VentasApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(VentasApplication.class, args);
+	}
 
 }
